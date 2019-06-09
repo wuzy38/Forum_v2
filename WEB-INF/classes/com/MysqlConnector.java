cf7318145026c7be36963992dce5b31cc4977e2c
Xu <540250339@qq.com>
2019-06-09 14:53:34 +0800

    update java code

--- WEB-INF/classes/com/MysqlConnector.java
+++ WEB-INF/classes/com/MysqlConnector.java
@@ -2,47 +2,74 @@ import java.util.*;
 import java.sql.*;
 // package com;
 
-public class MysqlConnector{
-    private String connStr;
+public class MysqlConnector {
+    // private connStr;
     private Connection con;
     private Statement stmt;
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        System.out.println(111);
         MysqlConnector test = new MysqlConnector();
-        
+        System.out.println(test.checkPassword("user_name999", "password999"));
     }
-    public MysqlConnector(){
-        connStr = "jdbc:mysql://localhost:3306/forum?serverTimezone=GMT" 
-        + "useUnicode=true&characterEncoding=utf-8";
-        String msg=""; 
-        String table="";
+
+    public MysqlConnector() {
+        String connStr = "jdbc:mysql://localhost:3306/forum?serverTimezone=GMT"
+                + "&useUnicode=true&characterEncoding=utf-8";
+        
+
+        con = null;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); //数据库驱动
-            con = DriverManager.getConnection(connStr, "root", "88720073"); 
-            stmt = con.createStatement();   //创建mysql对象
-            ResultSet result = stmt.executeQuery("select * from user"); //执行，返回结果
-            while(result.next()) {                  //cuscor移到下一个记录
-                table=result.getString("user_name"); //如果还有记录，就得到数据
-                System.out.println(table);
-            }
-            System.out.println("theme");
-            result=stmt.executeQuery("select * from theme");
-            while(result.next()) {               
-                table=result.getString("theme_id"); 
-                System.out.println(table);
-            }
-            result.close(); 
-            stmt.close();
-            con.close();
-        }
-        catch (Exception e){
-            msg = e.getMessage();
+            Class.forName("com.mysql.cj.jdbc.Driver"); // 数据库驱动
+            con = DriverManager.getConnection(connStr, "root", "88720073");
+        } catch (Exception e) {
+            String msg = e.getMessage();
+            // out.write(msg);
             System.out.println(msg);
         }
     }
-    
+
+    public boolean checkPassword(String user, String password) {
+        // stmt = con.createStatement(); //创建mysql对象
+        try{
+            String table = "";
+            String sql = "select * from user where user_name = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, user);
+            ResultSet result = ps.executeQuery();
+            while (result.next()) { // cuscor移到下一个记录
+                table = result.getString("user_password"); // 如果还有记录，就得到数据
+                if (table == password) {
+                    return true;
+                }
+            }
+            result.close();
+            con.close();
+            
+        } catch(Exception e){
+            String msg = e.getMessage();
+            // out.write(msg);
+            System.out.println(msg);
+        }
+        return false;
+        
+    }
+
     // public GetUserData(){
     // }
     // public Get
-    
+    private static List convertList(ResultSet rs) throws SQLException{
+        List list = new ArrayList();
+        ResultSetMetaData md = rs.getMetaData();//获取键名
+        int columnCount = md.getColumnCount();//获取行的数量
+        while (rs.next()) {
+        Map rowData = new HashMap();//声明Map
+        for (int i = 1; i <= columnCount; i++) {
+        rowData.put(md.getColumnName(i), rs.getObject(i));//获取键名及值
+        }
+        list.add(rowData);
+        }
+        return list;
+    }
 
 }
---
+++ sqlTest.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" import="java.util.*,java.sql.*"
+contentType="text/html; charset=utf-8"%>
+<h1>test</h1>
+<% 
+
+%>
+
